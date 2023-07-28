bebfb5450615efab7332c3bd5291dbe092e9aa94
Gilang Agung Saputra <gilangagungs20@gmail.com>
2023-07-28 17:50:18 +0700

    add second tutorial result from FreeCodeCamp

--- React_TypeScript/FreeCodeCamp/TypeScript_Tutorial_1/typescript-tutorial/src/App.tsx
+++ React_TypeScript/FreeCodeCamp/TypeScript_Tutorial_1/typescript-tutorial/src/App.tsx
@@ -36,14 +36,100 @@ let peopleInSchool: Person[];
 
 //======================================================================
 // make a function
-function printSomething(input: any) {
-  
+function printString(input: string): string {
+  console.log("Hello, your input is " + input);
+  return input;
 }
+
+printString("I can be developer using TypeScript and React haha");
+//======================================================================
+
+const printFullName = (name: string): string => {
+  return `Hello, ${name}`;
+};
+
+const result = printFullName("Agung");
+console.log(result); // Output: "Hello, John Doe"
+//console.log(printName("Gilang"));
+
+function printFullNameTwo(name: string): string{
+  return "Hello, you name is " + name;
+}
+let printMyName = printFullNameTwo("Gilang");
+console.log(printMyName);
+//======================================================================
+
+// INTERFACE
+
+interface IPersonData {
+  id: number,
+  name: string,
+  phone: number | string,
+  address?: string
+}
+
+interface IPerson {
+  name: string,
+  age: number,
+  phone: string,
+}
+
+interface IMan extends IPerson{
+  heigh: number
+}
+
+interface IJob {
+  jobName: string,
+  jobDesc: string
+}
+//======================================================================
+
+
+//======================================================================
+
+// Menggabungkan 2 Type (extending type)
+
+type Panjang = {
+  panjang: number;
+}
+
+type Persegi = Panjang & {
+  lebar: number;
+}
+
+let persegi: Persegi = {
+  panjang: 10,
+  lebar: 20
+}
+//======================================================================
+// An interface extends to type
+
+type PersonInSchool = {
+  id: number,
+  name: string,
+  age: number
+}
+
+interface IStudent extends PersonInSchool {
+  grade: number,
+  overAllScore: number
+}
+
+let studentOne: IStudent = {
+  id: 1,
+  name: "Niki",
+  age: 22,
+  grade: 3,
+  overAllScore: 90
+}
+
+console.log(studentOne);
+
 
 function App() {
   return (
     <div className="App">
-      Welcome to Typescript Tutorial by FreeCodeCamp
+      Gilang Learning TypeScript and React from FreeCodeCamp
     </div>
   );
 }
